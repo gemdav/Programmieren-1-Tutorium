dbb5c3dfecac907d8032b2604257dcacb6f93946
David Gemen <d.gemen@tu-bs.de>
2021-11-09 14:25:09 +0100

    data types example

---
+++ solutions/lesson_01_first_steps_with_java/showcases/DataTypes.java
@@ -0,0 +1,27 @@
+package solutions.lesson_01_first_steps_with_java.showcases;
+
+/**
+ * Data types showcase class
+ * 
+ * @author David Gemen
+ */
+public class DataTypes {
+
+    /**
+     * main function, program starts here
+     * 
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+        int x = 2;
+        double y = 1.5;
+
+        char c = 'f';
+        String str = "abcdefg";
+
+        boolean b = false;
+
+        System.out.println(x + y + c + str + b);
+    }
+}
