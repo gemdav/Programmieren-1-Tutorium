6b1fe00a99b73fe934dee26c567366198d3f9b91
David Gemen <d.gemen@tu-bs.de>
2021-11-25 13:34:32 +0100

    prime checker optimized

--- solutions/lesson_04_functions_classes_packages/exercises/prime/PrimeChecker.java
+++ solutions/lesson_04_functions_classes_packages/exercises/prime/PrimeChecker.java
@@ -17,7 +17,7 @@ public class PrimeChecker {
         if (number < 2) {
             return false;
         }
-        for (int i = number / 2; i > 1; i--) {
+        for (int i = 2; i <= Math.sqrt(number); i--) {
             if (number % i == 0) {
                 return false;
             }
