7f11e0813d2a08942f5a6cd251a3433a9e271a48
David Gemen <d.gemen@tu-bs.de>
2022-05-18 06:55:41 +0200

    bugfix

--- solutions/lesson_04_functions_classes_packages/exercises/prime/PrimeChecker.java
+++ solutions/lesson_04_functions_classes_packages/exercises/prime/PrimeChecker.java
@@ -17,7 +17,7 @@ public class PrimeChecker {
         if (number < 2) {
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(number); i--) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
                 return false;
             }
